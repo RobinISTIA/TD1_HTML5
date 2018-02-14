ae303b77a29b2e7a8593eda30d1c2ccd35c5d5e8
Unknown <robin.flavigny@free.fr>
2018-02-14 17:20:50 +0100

    Ajout TD3

---
+++ TD3/hello_server.js
@@ -0,0 +1,17 @@
+var PORT = 8080;
+
+var http = require('http');
+var swig = require('swig');
+
+var server = http.createServer(function(req, res) { 
+	res.writeHead(200, {'Content-Type': 'text/html'}); 
+    res.write(swig.renderFile('templates/home.tpl', { 
+    	name : 'Robin'
+	})
+);
+res.end(); 
+});
+
+server.listen(PORT);
+
+console.log('Server running on ' + PORT);
